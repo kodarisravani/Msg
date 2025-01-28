f6ec94ae0d992e96891af236cca188d6921c6b03
kodarisravani <kodarisravani3@gmail.com>
2025-01-29 05:28:26 +0530

    Add files via upload

---
+++ Msg.java
@@ -0,0 +1,11 @@
+
+public class Msg {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+         greetings();
+    }
+    public static void greetings(){
+        System.out.println("hello,world");
+    }
+}
+
